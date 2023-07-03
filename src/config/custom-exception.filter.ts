4fc4a42a8db73b4db00f51de5ba9391fa118c571
HanHyunsoo <gustn4563@gmail.com>
2023-07-04 01:54:27 +0900

    feat: Add Logger using Discord Webhook

--- src/config/custom-exception.filter.ts
+++ src/config/custom-exception.filter.ts
@@ -3,24 +3,49 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { CustomException } from '../common/exception/custom-execption';
+import { HttpAdapterHost } from '@nestjs/core';
+import { Webhook, MessageBuilder } from 'discord-webhook-node';
 
-@Catch(CustomException)
+@Catch()
 export class CustomExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-    const message = exception.message;
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
-    response.status(status).json({
-      statusCode: status,
-      message: message,
+  private readonly webhook = new Webhook(process.env.DISCORD_WEBHOOK_URL);
+
+  private readonly isProduction = process.env.NODE_ENV === 'production';
+
+  catch(exception: Error, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+    let errorParams = {
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal Server Error',
+    };
+
+    if (exception instanceof HttpException) {
+      errorParams = {
+        statusCode: exception.getStatus(),
+        message: exception.message,
+      };
+    }
+
+    if (errorParams.statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
+      this.isProduction
+        ? this.webhook.send(
+            new MessageBuilder().setDescription(exception.stack).setTimestamp(),
+          )
+        : console.log(exception.stack);
+    }
+
+    const responseBody = {
+      ...errorParams,
       timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, errorParams.statusCode);
   }
 }
