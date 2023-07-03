9a994aa46ec5b81a802cbbb19d8c52de179ed694
HanHyunsoo <gustn4563@gmail.com>
2023-07-03 13:49:21 +0900

    docs: Documentation for controller, dto

--- src/upload/dto/delete-image.dto.ts
+++ src/upload/dto/delete-image.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class DeleteImageDto {
   @ApiProperty({
-    description: 'S3 객체 URL',
+    description: '삭제할 S3 객체 URL',
     example: 'https://example-bucket.s3.region.amazonaws.com/test/1.png',
   })
   readonly s3Url: string;
--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -12,21 +12,53 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { parseFilePipe } from './util/image.filter';
 import { ImageResponseDto } from './dto/image-response.dto';
 import { DeleteImageDto } from './dto/delete-image.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateImageDto } from './dto/create-image.dto';
 
-@Controller('upload')
+@Controller('uploads')
+@ApiTags('uploads')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiOperation({
+    summary: '이미지 업로드',
+    description: '이미지를 업로드합니다. 원래 이름 대신 uuid로 저장됩니다.',
+  })
+  @ApiCreatedResponse({
+    description: '이미지 업로드 성공',
+    type: ImageResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: '이미지 업로드에서 문제가 생김(용량, 파일 형식)',
+  })
   async create(
     @UploadedFile(parseFilePipe)
     file: Express.Multer.File,
+    @Body()
+    createImageDto: CreateImageDto,
   ): Promise<ImageResponseDto> {
     return this.uploadService.create('test', file);
   }
 
   @Delete()
+  @ApiOperation({
+    summary: '이미지 삭제',
+    description: 'S3 객체 URL을 이용해서 이미지를 삭제합니다.',
+  })
+  @ApiOkResponse({
+    description: '이미지 삭제 성공',
+  })
   remove(@Body() deleteImageDto: DeleteImageDto): Promise<void> {
     return this.uploadService.remove(deleteImageDto.s3Url);
   }
